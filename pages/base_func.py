a3d971ef8f7caa24a4d842ec8eda01041966b7e7
Ugarov Danil <danilygarov06@gmail.com>
2023-01-27 00:46:04 +0300

    add in BasePage method is_find_element

--- pages/base_func.py
+++ pages/base_func.py
@@ -2,9 +2,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout = 5):
         self.browser = browser
         self.url = url
+        self.browser.implisitly_wait(timeout)
         
     def open(self):
         self.browser.get(self.url)
+        
+    def is_find_element(self, how, what):
+        try:
+            self.browser.find_elements(how, what)
+        except :
+            return False
+        return True
