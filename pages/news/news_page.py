6f97fa521186000ff44a804affd939f13f1ebe71
Ugarov Danil <danilygarov06@gmail.com>
2023-01-26 23:49:24 +0300

    add function 'is_btn_news_page_change_active'

--- pages/news/news_page.py
+++ pages/news/news_page.py
@@ -11,22 +11,26 @@ class NewsPage(BasePage):
     def is_news_list_present(self):
         try:
             self._news_list = self.browser.find_elements(*NewsPageLocators.NEWS_LIST)
-        except():
+        except :
             return False
         return True
         
     def change_news_pageination(self, number):
     	self._page_clickabel = number
-        pagination = self.browser.find_elements(*NewsPageLocators.POGINATION_NEWS)
-        if(pagination[number].get_attribute('active')):
+        pagination = self.browser.find_elements(*NewsPageLocators.PAGINATION_NEWS)
+        if pagination[number].get_attribute('active') == 'true':
             print('Click on active page button')
         pagination[numder].click()
                 
-    def is_btn_news_page_change_active(self):
+    def is_btn_news_page_change_active(self, number):
+        pagination = self.browser.find_elements(*NewsPageLoators.PAGINATION_NEWS)
+        if pagination[number].get_attribute('active') == 'true':
+            return True
+        return False
         
                 
     def is_news_page_changes(self):
         new_news_list = WebDriverWait(self.browser, 5).until(EC.element_to_be_clickable(NewsPageLocators.NEWS_LIST))
-        if (self._news_list != new_news_list):
+        if self._news_list != new_news_list:
             return True
         return False
