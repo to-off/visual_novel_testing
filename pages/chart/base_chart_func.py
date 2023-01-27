d0cc47de169a569dbbd0dd0b282094f3985e3b22
Sheeshelty <87362691+Sheslty@users.noreply.github.com>
2023-01-27 04:06:24 +0300

    delete pages

--- pages/chart/base_chart_func.py
+++
@@ -1,7 +0,0 @@
-from pages.general_func import GeneralFunction
-from .locators import BaseChartLocators
-
-class BaseChartFunction(GeneralFunction):
-    def go_to_chart_page(self):
-        chart_main = self.browser.find_element(*ChartPageLocators.CHART_LOCATOR)
-        chart_main.click()
