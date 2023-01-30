e765e391122c254318a882cf054a1453b222025a
SanZeZZ <110095967+SanZeZZ@users.noreply.github.com>
2023-01-30 17:48:34 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture(scope="function")
 def browser():
@@ -11,3 +14,10 @@ def browser():
     browser.quit()
     
     
+@pytest.fixture
+def driver():
+    driver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=driver_service)
+    driver.maximize_window()
+    yield driver
+    driver.quit()
