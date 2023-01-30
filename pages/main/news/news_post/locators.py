de8dcad9e9265195d8a9391fd2019e54a890a897
to-off <92836884+to-off@users.noreply.github.com>
2023-01-30 15:41:34 +0300

    Delete pages/main directory
    wait code review

--- pages/main/news/news_post/locators.py
+++
@@ -1,6 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class NewsPostPageLocators():
-    MAIN = (By.CSS_SELECTOR, 'div>h3>a:first-of-type')
-    NEWS = (By.CSS_SELECTOR, 'div>h3>a:nth-child(2)')    	
-    REFERENCES_ON_DESCRIPTION_NEWS = (By.CSS_SELECTOR, 'div.row:nth-child(2)  > div:nth-child(1) a')
