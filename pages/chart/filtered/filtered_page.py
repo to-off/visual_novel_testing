f240775d01b2b730def4f41392340a8cad555805
Ugarov Danil <danilygarov06@gmail.com>
2023-01-31 13:58:09 +0300

    Delete chart branch from test_main branch

--- pages/chart/filtered/filtered_page.py
+++
@@ -1,6 +0,0 @@
-from pages.chart.branches_func import BranchesFunction
-from pages.chart.chart_page import ChartPage
-
-class FilteredPage(ChartPage, BranchesFunction):
-    def __init__(self, *args, **kwargs):
-        super(FilteredPage, self).__init__(*args, **kwargs)
