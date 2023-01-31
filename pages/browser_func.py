48de04503f0036ebf5abbcac40274aaed4c26470
to-off <92836884+to-off@users.noreply.github.com>
2023-01-31 04:31:49 +0300

    Update browser_func.py
    Change find_element in  is_element_present

--- pages/browser_func.py
+++ pages/browser_func.py
@@ -1,17 +1,49 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
 '''
 Класс с основными методами браузера и его определением
 '''
-class BrowserFunction():
-    def __init__(self, browser, url):
+
+
+class BrowserFunction:
+    def __init__(self, browser, url='', timeout=5):
         self.browser = browser
         self.url = url
-    def find_element(self, locator,time=10):
-        return WebDriverWait(self.driver,time).until(EC.presence_of_element_located(locator),
-                                                      message=f"Can't find element by locator {locator}")
-    def find_elements(self, locator,time=10):
-        return WebDriverWait(self.driver,time).until(EC.presence_of_all_elements_located(locator),
-                                                      message=f"Can't find elements by locator {locator}")
+        self.browser.implicitly_wait(timeout)
+
+    def is_element_present(self, how, what, time=5):
+        try:
+            WebDriverWait(self.browser, time).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    def is_elements_present(self, how, what, time=5):
+        try:
+            WebDriverWait(self.browser, time).until(EC.presence_of_all_elements_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
     def open(self):
         self.browser.get(self.url)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
